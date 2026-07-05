ec07cbb74a0b168146da0e90966110f8ff0c7c51
StevenTanDEV <steventangamedev@gmail.com>
2026-07-06 10:32:07 +1200

    feat: Contact Icons on Footer

--- layout.js
+++ layout.js
@@ -26,5 +26,10 @@ document.getElementById("footer").innerHTML = `
 	<ul class="copyright">
 		<li>&copy; 2026 Steven Tan</li>
 	</ul>
+	<ul class="icons">
+		<li><a href="https://www.linkedin.com/in/steventan-nz/" class="icon brands style2 fa-linkedin"><span class="label">LinkedIn</span></a></li>
+		<li><a href="https://steventan.itch.io/" class="icon brands style2 fa-itch-io"><span class="label">Itch.io</span></a></li>
+		<li><a href="mailto:tan2001steven@hotmail.com" class="icon solid style2 fa-envelope"><span class="label">Email</span></a></li>
+	</ul>
 </div>
 `;
